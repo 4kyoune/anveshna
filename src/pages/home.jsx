17e8ade8704bb4e17449283b0bfd43b7920bc4d5
4kyoune <wangyyputra@gmail.com>
2024-06-03 15:38:59 +0700

    Update home.jsx

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,12 +4,6 @@ import { fetchTrendingAnime, fetchPopularAnime, fetchTopAirAnime} from '../hooks
 import CardGrid from '../components/cards/cardItem';
 import HomeSideBar from '../components/home/topAir';
 
-<div class="row">
-  <div class="Home"></div>
-  <div class="Manga"></div>
-  <div class="novel"></div>
-</div>
-
 const Home = () => {
   window.scrollTo(0, 0);
   const [activeTab, setActiveTab] = useState('TRENDING');
